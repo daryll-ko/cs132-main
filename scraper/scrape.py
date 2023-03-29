8617650aa208b423f11aab9f6d24937dde9edc26
Daryll Ko <daryll.ko@gmail.com>
2023-03-29 20:56:30 +0800

    feat: allow writing to a csv file

--- scraper/scrape.py
+++ scraper/scrape.py
@@ -1,11 +1,19 @@
 import os
 import twint
 
+OUTPUT_FILE = f"{os.path.dirname(__file__)}/output.csv"
+
+os.remove(OUTPUT_FILE)
+
 c = twint.Config()
 
 c.Username = "jarredsumner"
 c.Search = "zig"
-c.Output = f"{os.path.dirname(__file__)}/output.txt"
+
+c.Stats = True
 c.Hide_output = True
+c.Store_csv = True
+c.Custom["tweet"] = ["id", "username"]
+c.Output = OUTPUT_FILE
 
 twint.run.Search(c)
