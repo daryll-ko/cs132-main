fe75873d156f1c694d42c5c7c17a80354367221c
Daryll Ko <daryll.ko@gmail.com>
2023-03-29 21:54:58 +0800

    feat: add argparse

--- scraper/scrape.py
+++ scraper/scrape.py
@@ -1,20 +1,47 @@
+import argparse
 import os
 import twint
 
 OUTPUT_FILE = os.path.join(os.path.dirname(__file__), "output.csv")
 
-if os.path.exists(OUTPUT_FILE):
-    os.remove(OUTPUT_FILE)
 
-c = twint.Config()
+def main():
+    parser = argparse.ArgumentParser()
 
-c.Username = "jarredsumner"
-c.Search = "zig"
+    parser.add_argument("-u", "--username", default="jarredsumner")
+    parser.add_argument("-s", "--search", default="zig")
+    parser.add_argument("-l", "--limit", default="200")
 
-c.User_full = True
-c.Hide_output = True
-c.Store_csv = True
-c.Custom["tweet"] = ["username", "id", "name", "tweet", "created_at", "mentions", "urls", "photos", "likes_count", "replies_count", "retweets_count"]
-c.Output = OUTPUT_FILE
+    args = parser.parse_args()
 
-twint.run.Search(c)
+    if os.path.exists(OUTPUT_FILE):
+        os.remove(OUTPUT_FILE)
+
+    c = twint.Config()
+
+    c.Username = args.username
+    c.Search = args.search
+    c.Limit = int(args.limit)
+
+    c.Hide_output = True
+    c.Store_csv = True
+    c.Custom["tweet"] = [
+        "username",
+        "id",
+        "name",
+        "tweet",
+        "created_at",
+        "mentions",
+        "urls",
+        "photos",
+        "likes_count",
+        "replies_count",
+        "retweets_count"
+    ]
+    c.Output = OUTPUT_FILE
+
+    twint.run.Search(c)
+
+
+if __name__ == "__main__":
+    main()
