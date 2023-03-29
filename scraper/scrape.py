1900a52f3506beefc93996e31f30d7a7db505e15
Daryll Ko <daryll.ko@gmail.com>
2023-03-29 20:25:16 +0800

    feat: make file output inherent

--- scraper/scrape.py
+++ scraper/scrape.py
@@ -1,8 +1,11 @@
+import os
 import twint
 
 c = twint.Config()
 
 c.Username = "jarredsumner"
 c.Search = "zig"
+c.Output = f"{os.path.dirname(__file__)}/output.txt"
+c.Hide_output = True
 
 twint.run.Search(c)
