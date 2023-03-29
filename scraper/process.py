52a2b0c26a1d68ffb64b3ea39ad2c6fd4e2f3a7d
Daryll Ko <daryll.ko@gmail.com>
2023-03-30 00:02:28 +0800

    feat: automatically extract basic account info

--- scraper/process.py
+++ scraper/process.py
@@ -1,6 +1,7 @@
 import os
 import pandas as pd
 import re
+import subprocess
 
 INPUT_FILE = os.path.join(os.path.dirname(__file__), "output.csv")
 OUTPUT_FILE = os.path.join(os.path.dirname(__file__), "processed.csv")
@@ -25,23 +26,62 @@ def date_posted(row):
     return f"{day}/{month}/{year} {hour}:{minute}"
 
 
+user_info_hash = {}
+
+
+def generate_user_info(row):
+    if row.username not in user_info_hash:
+        result = subprocess.getoutput(f"snscrape --jsonl --with-entity --max-results 0 twitter-user {row.username}")
+        df = pd.read_json(result, lines=True)
+        user_info_hash[row.username] = df.to_dict()
+
+
+def account_bio(row):
+    return user_info_hash[row.username]["description"][0]
+
+
+def joined(row):
+    date_string = user_info_hash[row.username]["created"][0]
+    result = re.search(r"(\d{4})-(\d{2})-(\d{2}).*", date_string)
+    year, month, _ = result.groups()
+    return f"{month}/{year[2:]}"
+
+
+def following(row):
+    return user_info_hash[row.username]["friendsCount"][0]
+
+
+def followers(row):
+    return user_info_hash[row.username]["followersCount"][0]
+
+
+def location(row):
+    return user_info_hash[row.username]["location"][0]
+
+
 def main():
     if not os.path.exists(INPUT_FILE):
         print("Make sure you've run scrape.py!")
         exit()
     df = pd.read_csv(INPUT_FILE)
 
+    for _, row in df.iterrows():
+        generate_user_info(row)
+
     df["ID"] = df.apply(lambda row: row_id(row), axis=1)
     df["Tweet URL"] = df.apply(lambda row: tweet_url(row), axis=1)
     df["Account handle"] = df.apply(lambda row: account_handle(row), axis=1)
     df["Date posted"] = df.apply(lambda row: date_posted(row), axis=1)
-    
+    df["Account bio"] = df.apply(lambda row: account_bio(row), axis=1)
+    df["Joined"] = df.apply(lambda row: joined(row), axis=1)
+    df["Following"] = df.apply(lambda row: following(row), axis=1)
+    df["Followers"] = df.apply(lambda row: followers(row), axis=1)
+    df["Location"] = df.apply(lambda row: location(row), axis=1)
+
     df = df.assign(Group="09")
     df = df.assign(Collector="Ko, Daryll")
     df = df.assign(Category="RBRD")
     df = df.assign(Topic="Leni's incompetence as VP")
-
-    df["Keywords"] = ""
 
     df["Account name"] = df["name"]
     df["Tweet"] = df["tweet"]
@@ -59,28 +99,28 @@ def main():
         # Keywords
         "Account handle",
         "Account name",
-        # Account bio
+        "Account bio",
         # Account type
-        # Joined
-        # Following
-        # Followers
-        # Location
+        "Joined",
+        "Following",
+        "Followers",
+        "Location",
         "Tweet",
-        # [Tweet Translated]
+        #   [Tweet Translated]
         # Tweet Type
         "Date posted",
-        # [Screenshot]
+        #   [Screenshot]
         # Content type
         "Likes",
         "Replies",
         "Retweets",
-        # [Quote Tweets]
-        # [Views]
-        # [Rating]
+        #   [Quote Tweets]
+        #   [Views]
+        #   [Rating]
         # Reasoning
-        # [Remarks]
-        # [Reviewer]
-        # [Review]
+        #   [Remarks]
+        #   [Reviewer]
+        #   [Review]
     ]
 
     final_df = df[final_rows].copy()
