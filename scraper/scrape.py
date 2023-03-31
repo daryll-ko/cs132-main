ee2c352f9ab10113ee96137177fb405c08c5ef83
Daryll Ko <daryll.ko@gmail.com>
2023-03-31 11:07:20 +0800

    feat: add fromYear and toYear CLI arguments for `scrape`

--- scraper/scrape.py
+++ scraper/scrape.py
@@ -11,11 +11,17 @@ def main():
     parser.add_argument("-u", "--username")
     parser.add_argument("-s", "--search")
     parser.add_argument("-l", "--limit", default="100")
+    parser.add_argument("-f", "--fromYear", default="2016")
+    parser.add_argument("-t", "--toYear", default="2022")
 
     args = parser.parse_args()
 
     if not args.search:
         print("Include a search term!")
+        exit()
+
+    if int(args.fromYear) > int(args.toYear):
+        print("Make sure -f is less than -t!")
         exit()
 
     if os.path.exists(OUTPUT_FILE):
@@ -31,6 +37,9 @@ def main():
     c.Hide_output = True
     c.Store_csv = True
     c.Output = OUTPUT_FILE
+
+    c.Since = f"{args.fromYear}-01-01"
+    c.Until = f"{args.toYear}-12-31"
 
     twint.run.Search(c)
 
